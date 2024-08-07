93b493d30ff87fa10b194337c239c3c8e8ce11c3
Zahid <zahid_sanchez@ciencias.unam.mx>
2024-08-07 14:58:42 -0600

    [UPDATE] write functions

--- src/utils/functions.py
+++ src/utils/functions.py
@@ -1,23 +1,24 @@
 import time
+import sys
 
 def slow_print(text):
-    for x in text:
-        time.sleep(0.04)
-        print(x,end='')
-    print('')
+    for x in text + '\n':
+        sys.stdout.write(x)
+        sys.stdout.flush()
+        time.sleep(0.02)
     time.sleep(1)
     
         
 def slow_talk(text):
-    for x in text:
-        time.sleep(0.1)
-        print(x,end='')
-    print('')
+    for x in text + '\n':
+        sys.stdout.write(x)
+        sys.stdout.flush()
+        time.sleep(0.08)
     time.sleep(1)
         
 def scream(text):
-    for x in text:
+    for x in text+ '\n':
+        sys.stdout.write(x.upper())
+        sys.stdout.flush()
         time.sleep(0.4)
-        print(x.upper(),end='')
-    print('')
     time.sleep(1)
